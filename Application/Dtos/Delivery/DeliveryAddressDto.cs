bfaabeb8c42e1f2d4dc2d550e2606754299f82e6
Rafael Silva <ainbot6@gmail.com>
2024-02-03 01:12:07 -0300

    adding Data Transfer Object to the delivery address

---
+++ Application/Dtos/Delivery/DeliveryAddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Dtos.Delivery;
+
+public class DeliveryAddressDto
+{
+    public string ZipCode { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string Complement { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Neighborhood { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+}
---
+++ Application/Dtos/Delivery/UserDeliveryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dtos.Delivery;
+
+public class UserDeliveryDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string SSN { get; set; } = string.Empty;
+}
