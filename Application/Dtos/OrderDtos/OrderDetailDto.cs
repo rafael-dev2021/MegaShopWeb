20d8819f9acf66a5f515514c811cf7de4cde1978
Rafael Silva <ainbot6@gmail.com>
2024-02-03 01:13:00 -0300

    adding Data Transfer Object to order

---
+++ Application/Dtos/OrderDtos/OrderDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos.PaymentsDto;
+
+namespace Application.Dtos.OrderDtos;
+
+public class OrderDetailDto
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public int ProductId { get; set; }
+    public ProductDto Product { get; set; }
+    public int OrderId { get; set; }
+    public OrderDto Order { get; set; }
+    public CreditCardDto CreditCard { get; set; }
+}
---
+++ Application/Dtos/OrderDtos/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿using Application.Dtos.Delivery;
+using Application.Dtos.PaymentsDto;
+
+namespace Application.Dtos.OrderDtos;
+
+public class OrderDto
+{
+    public int Id { get; set; }
+    public decimal TotalOrder { get; set; }
+    public int TotalItemsOrder { get; set; }
+    public DateTime ConfirmedOrder { get; set; }
+    public DateTime DispatchedOrder { get; set; }
+    public DateTime RequestReceived { get; set; }
+    public List<OrderDetailDto> OrderDetails { get; }
+    public DeliveryAddressDto DeliveryAddress { get; set; }
+    public UserDeliveryDto UserDelivery { get; set; }
+    public PaymentMethodDto PaymentMethod{ get; set; }
+
+}
