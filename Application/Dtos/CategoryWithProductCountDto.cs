8409a586a0824ca69490e9272dab687a40d72ee7
Rafael Silva <ainbot6@gmail.com>
2024-02-03 01:11:21 -0300

    adding count categories with products

---
+++ Application/Dtos/CategoryWithProductCountDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Dtos;
+
+public class CategoryWithProductCountDto
+{
+    public string CategoryName { get; set; } = string.Empty;
+    public int ProductCount { get; set; }
+}
