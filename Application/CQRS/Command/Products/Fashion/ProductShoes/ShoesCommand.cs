a753c8ad3ef2b6e745e35f34000b92dfb430e1f8
Rafael Silva <ainbot6@gmail.com>
2024-02-03 01:09:15 -0300

    changing class name

---
+++ Application/CQRS/Command/Products/Fashion/ProductShoes/ShoesCommand.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities.Products.Fashion.ProductShoes;
+using Domain.Entities.Valuables.ProductValuables;
+using MediatR;
+
+namespace Application.CQRS.Command.Products.Fashion.ProductShoes;
+
+public class ShoesCommand : IRequest<Shoes>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string> Images { get; set; }
+    public int Stock { get; set; }
+    public ProductDataOV ProductDataObjectValue { get; set; }
+    public ProductFlagsOV ProductFlagsObjectValue { get; set; }
+    public ProductPriceOV ProductPriceObjectValue { get; set; }
+    public ProductSpecificationsOV ProductSpecificationsObjectValue { get; set; }
+    public ProductWarrantyOV ProductWarrantyObjectValue { get; set; }
+    public ShoesDesignOV ShoesDesignObjectValue { get; set; }
+    public ShoesGeneralFeaturesOV ShoesGeneralFeaturesObjectValue { get; set; }
+    public ShoesMaterialsOV ShoesMaterialsObjectValue { get; set; }
+    public ShoesSpecificationsOV ShoesSpecificationsObjectValue { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ Application/CQRS/Command/Products/Fashion/ProductShoes/UpdateShoesCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.CQRS.Command.Products.Fashion.ProductShoes;
+
+public class UpdateShoesCommand : ShoesCommand
+{
+    public int Id { get; set; }
+}
