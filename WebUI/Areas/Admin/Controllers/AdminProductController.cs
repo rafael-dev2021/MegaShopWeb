a6a06ab7f81dc09ba01f6d40bc17c4662cfdaa2b
Rafael Silva <ainbot6@gmail.com>
2024-02-08 21:24:41 -0300

    add the AdminProduct controller to the administration area, and its CRUD, for manipulations with ADMIN permission

---
+++ WebUI/Areas/Admin/Controllers/AdminProductController.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services.Entities.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class AdminProductController(IProductDtoService productDtoService) : Controller
+{
+    private readonly IProductDtoService _productDtoService = productDtoService;
+
+    public async Task<IActionResult> Index() => View(await _productDtoService.GetProductsDtoAsync());
+}
